5d6fd740f89820836042860bf3efca7395248d1f
torrent benoit <torrentdu64@gmail.com>
2018-08-09 15:34:50 +1200

    try to build app

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import ChannelList from '../containers/channel_list';
+import MessageList from '../containers/message_list';
 
 const App = () => {
   return (
-    <div className="app">
-      <p>React + Redux starter</p>
+    <div className="messaging-wrapper">
+      <div className="logo-container">
+        <img className="messaging-logo" src="assets/images/logo.svg" alt="logo" />
+      </div>
+      <ChannelList />
+      <MessageList />
     </div>
+
   );
 };
 
